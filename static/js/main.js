4caa0df9f99e43db012a0e702979cd7039904d5b
Ram Dettmer <ramdettmer751@gmail.com>
2020-05-02 03:25:41 -0700

    designed blog

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,22 @@
+// NAV
+$(document).ready(function() {
+            
+  // Check for click events on the navbar burger icon
+      $(".navbar-burger").click(function() {
+      
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          $(".navbar-burger").toggleClass("is-active");
+          $(".navbar-menu").toggleClass("is-active");
+      
+      });
+
+      $('.sub-menu').click(function(){
+      $('.sub').toggleClass('open');
+       });
+  });
+
+
+
 // BLOG SLIDER
 $('.blog-slider').slick({
     dots: true,
